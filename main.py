898c5973cf02059c5b6ccca68af6f887e9700151
joaoCzr <joaogemelli18@gmail.com>
2025-10-29 08:55:29 -0400

    projetoflaskV0.2

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask
-
-joao_app = Flask(__name__)
-
-@joao_app.route("/")
-def home():
-    return "Olá, este é o primeiro teste da framework!!"
 
 
-joao_app.run()
+from flask import Flask 
+
+app_joao = Flask(__name__)  
+
+
+@app_joao.route('/Ola')  
+
+
+def raiz():
+    return 'Olá, professora!'
+
+if __name__ == "__main__": 
+    app_joao.run()  

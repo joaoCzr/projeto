1cdff2bb734b8f5d1ee707ba547f1efcaa8bea09
joaoCzr <joaogemelli18@gmail.com>
2025-10-29 10:11:36 -0400

    projetoflaskV0.5( pasta de template e arquivos html criados)

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
+from flask import Flask, render_template
 
-from flask import Flask  
+app_joao = Flask(__name__ , template_folder= 'templates')
 
-app_joao = Flask (__name__) 
 
-@app_joao.route('/')    #rota para solicitação web
-@app_joao.route('/rota1') #rota para solicitação web
-def rota1():  # função a ser executa quando chamar rotas acima ('/') e ('/rota1')
-    return 'teste da rota 1'
+@app_joao.route("/") 
+def homepage():          
+    return render_template ("homepage.html")
 
-@app_joao.route('/rota2')
-def rota2():
-    resposta = "<H3> teste da rota 2 <H3>"
-    return resposta
+@app_joao.route("/contato")
+def contato():
+    return render_template("contato.html") 
 
-#função que não pertence a nenhuma rota. 
-def saudacoes (nome): 
-    return f'Olá, {nome}'
-
-if __name__ == "__main__" :
-    app_joao.run(port = 8000)  #executa aplicação
-
+if __name__ == "__main__":
+     app_joao.run(port = 8000)
+                                

e00600eef33eb12569fe8308f4f0f7f11c72e658
joaoCzr <joaogemelli18@gmail.com>
2025-10-29 11:22:20 -0400

    projetoflaskV0.6( Arquivos index e usuario.html criados)

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from flask import Flask, render_template
 
-app_joao = Flask(__name__ , template_folder= 'templates')
+app_joao = Flask(__name__, template_folder='templates')
 
-
-@app_joao.route("/") 
+@app_joao.route("/")  
 def homepage():          
-    return render_template ("homepage.html")
+    return render_template("homepage.html")
 
 @app_joao.route("/contato")
 def contato():
     return render_template("contato.html") 
 
+@app_joao.route("/index")
+def indice():
+    dados_usu = {"nome": "João"}
+    return render_template("index.html", **dados_usu)
+
+@app_joao.route("/usuario")
+def dados_usuario():
+    dados_usu = {"nome": "João", "profissao": "Estudante", "disciplina":"Desenvolvimento Web III"}
+    return render_template("usuario.html", dados=dados_usu)
+
 if __name__ == "__main__":
-     app_joao.run(port = 8000)
-                                
+    app_joao.run(port=8000, debug=True)
